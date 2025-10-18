a435c8e1cd23acebbe59564055ae9db3311cacde
Gregorio Jaca <grego.repollo@gmail.com>
2025-10-18 13:07:40 +0200

    put badcompany logo into browser tab

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { AuthProvider } from '../contexts/AuthContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Head>
+        {/* Favicon shown in browser tab - file placed in /public */}
+        <link rel="icon" href="/badcompany_logo1.jpg" />
+        <meta name="theme-color" content="#0f172a" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
